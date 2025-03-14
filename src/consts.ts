eefab6639e366e6c310cc4aa67b7d7f73749176f
Profe Matias Prestes <profematiasprestes@gmail.com>
2025-03-14 15:14:19 -0300

    2

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'UTU BACHILLERATO TECNOLÓGICO';
+export const SITE_TITLE = 'Matias Prestes - UTU';
+export const SITE_URL = 'https://profemprestes.github.io/';
+export const SITE_LOGO = 'https://profemprestes.github.io/img/logos/logofinal3.webp';
+export const SITE_NAME = 'Matias Prestes';
+export const SITE_TWITTER = '@profemprestes';
+export const SITE_GITHUB = 'profemprestes';
 export const SITE_DESCRIPTION = 'Portal Educativo de UTU - Tecnologías de la Información';

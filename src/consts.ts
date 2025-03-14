a9d979ee8ee599eb72d3eeadecc529745b595850
profemprestes <profematiasprestes@gmail.com>
2025-03-14 14:25:53 -0300

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'UTU BACHILLERATO TECNOLÓGICO';
+export const SITE_DESCRIPTION = 'Portal Educativo de UTU - Tecnologías de la Información';
